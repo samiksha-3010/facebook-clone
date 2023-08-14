ea3915289f75b1ebf269a674f3c288b4bd8d3c4a
samiksha-3010 <samiksha30102@gmail.com>
2023-08-14 23:11:40 +0530

    updates all

--- facebook/src/Component/Login.jsx
+++ facebook/src/Component/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import "./Login.css"
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+
+  const router= useNavigate();
   return (
-    <div>Login</div>
+
+    <div className='style-login'>
+      <form>
+        <label>Email:</label><br/>
+        <input className='email-password' type='text'placeholder='email adress and phone number'/><br/>
+        <label>Password:</label><br/>
+        <input className='email-password' type='password' placeholder='**********'/><br/>
+        <input className='submit' type='submit' value='Log in'/><br/>
+        <p className='password'>Forgetten Password ?</p>
+        <button onClick={() =>router('/Register')} className='button'>Creat New Acount</button>
+      </form>
+
+
+    </div>
   )
 }
 
--- facebook/src/Component/Register.jsx
+++ facebook/src/Component/Register.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import "./Register.css"
 
 const Register = () => {
   return (
-    <div>Register</div>
+
+      
+    
+    <div className='style-reg'>
+     <h2>Sign up</h2>
+     <p>it,s quick and easy</p>
+
+      <form>
+        <div className='first-style'>
+         <div> <input type='text' placeholder=' First  Name'/></div>
+         <div>  <input type='text' placeholder=' Last Name'/></div>
+
+        </div>
+       
+        <input className='mobile-password' type='text' placeholder='Mobile Number or email'/><br/>
+        <input  className='mobile-password'  type='text' placeholder='New Password'/><br/>
+
+        <b>Dath of Birth</b>
+         <div className='date-birth'>
+          <div>
+            <p>15</p>
+          </div>
+          <div>
+            <p>Aug</p>
+          </div>
+          <div>
+            <p>2023</p>
+          </div>
+         </div>
+
+         <b>Gender</b>
+         <div className='date-birth'>
+          <div>
+            <p>Female</p>
+          </div>
+          <div>
+            <p>Male</p>
+          </div>
+          <div>
+            <p>Custom</p>
+          </div>
+         </div>
+         <p></p>
+         <input className='submit-register' type='Sumbit' value='Sigh in'/>
+         <p style={{color: "blue" , marginLeft: "10px"}}>All Ready Have an Acount ?</p>
+
+
+         
+      </form>
+    </div>
   )
 }
 
